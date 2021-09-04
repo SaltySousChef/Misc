7221571971b2c8a37f974ef5935be3f13e2fced7
SaltySousChef <hossein@funhaus.io>
2021-09-04 20:22:39 +0800

    Remove subnet block due to GCP connection issues

--- iptables.sh
+++ iptables.sh
@@ -25,15 +25,15 @@ sudo iptables -t mangle -A PREROUTING -p tcp --tcp-flags ACK,URG URG -j DROP
 sudo iptables -t mangle -A PREROUTING -p tcp --tcp-flags ACK,PSH PSH -j DROP
 sudo iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL NONE -j DROP
 
-echo "Blocking requests from private subnets"
-sudo iptables -t mangle -A PREROUTING -s 224.0.0.0/3 -j DROP
-sudo iptables -t mangle -A PREROUTING -s 172.16.0.0/12 -j DROP 
-sudo iptables -t mangle -A PREROUTING -s 192.0.2.0/24 -j DROP 
-sudo iptables -t mangle -A PREROUTING -s 192.168.0.0/16 -j DROP 
-sudo iptables -t mangle -A PREROUTING -s 10.0.0.0/8 -j DROP 
-sudo iptables -t mangle -A PREROUTING -s 0.0.0.0/8 -j DROP 
-sudo iptables -t mangle -A PREROUTING -s 240.0.0.0/5 -j DROP 
-sudo iptables -t mangle -A PREROUTING -s 127.0.0.0/8 ! -i lo -j DROP
+# echo "Blocking requests from private subnets"
+# sudo iptables -t mangle -A PREROUTING -s 224.0.0.0/3 -j DROP
+# sudo iptables -t mangle -A PREROUTING -s 172.16.0.0/12 -j DROP 
+# sudo iptables -t mangle -A PREROUTING -s 192.0.2.0/24 -j DROP 
+# sudo iptables -t mangle -A PREROUTING -s 192.168.0.0/16 -j DROP 
+# sudo iptables -t mangle -A PREROUTING -s 10.0.0.0/8 -j DROP 
+# sudo iptables -t mangle -A PREROUTING -s 0.0.0.0/8 -j DROP 
+# sudo iptables -t mangle -A PREROUTING -s 240.0.0.0/5 -j DROP 
+# sudo iptables -t mangle -A PREROUTING -s 127.0.0.0/8 ! -i lo -j DROP
 
 echo "Blocking ICMP packets"
 sudo iptables -t mangle -A PREROUTING -p icmp -j DROP
