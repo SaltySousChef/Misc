f4a179812b7b9d134b9f28dc47c640234859d2d0
SaltySousChef <hossein@funhaus.io>
2021-09-06 19:03:49 +0800

    Update go and bug fixes
    - Updated go to 1.17
    - Tested on Ubuntu 20.04

--- regen-install.sh
+++ regen-install.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
-# install go (double escape on variables required to get them all the way there intact)
+# install dependencies
 cd ~/
 sudo apt update
 sudo apt install build-essential jq -y
-wget https://dl.google.com/go/go1.15.6.linux-amd64.tar.gz
-tar -xvf go1.15.6.linux-amd64.tar.gz
+sudo apt install expect -y
+wget https://golang.org/dl/go1.17.linux-amd64.tar.gz
+tar -xvf go1.17.linux-amd64.tar.gz
 sudo mv go /usr/local
 echo "" >> ~/.profile
 echo 'export GOPATH=$HOME/go' >> ~/.profile
 echo 'export GOROOT=/usr/local/go' >> ~/.profile
 echo 'export GOBIN=$GOPATH/bin' >> ~/.profile
 echo 'export PATH=$PATH:/usr/local/go/bin:$GOBIN' >> ~/.profile
-rm go1.15.6.linux-amd64.tar.gz
+rm go1.17.linux-amd64.tar.gz
 
-# refresh session to use go paths
+# refresh env
 . ~/.profile
+
+# check paths are set
+if [ -z "$GOPATH" ]
+then
+      echo "\$GOPATH not set! The environment was not configured correctly..."
+      exit 1
+fi
 
 # install regen-cli
 git clone https://github.com/regen-network/regen-ledger $GOPATH/src/github.com/regen-network/regen-ledger
@@ -24,17 +32,16 @@ git fetch
 git checkout v1.0.0
 make install
 
-# init the validator
+# generate ~/.regen folder (regen-1 is the current the main chain)
 regen init --chain-id regen-1 regen-validator
 
 # add genesis file (required to create keys)
 curl -s https://raw.githubusercontent.com/regen-network/mainnet/main/regen-1/genesis.json > ~/.regen/config/genesis.json
 
 # create keychain with random base64 password and generate primary wallet
-sudo apt install expect -y
 echo '#! /usr/bin/expect -f
 set PASSWORD [lindex $argv 0];
-spawn /home/node_runner/go/bin/regen keys add regen-wallet
+spawn regen keys add regen-wallet
 expect "Enter keyring passphrase:"
 send -- "$PASSWORD\r"
 expect "Re-enter keyring passphrase:"
@@ -47,21 +54,21 @@ cd ~/
 sed -i '1,2d' wallet.txt && sed -i 's/Re-enter keyring passphrase:/Keyring passphrase: /' wallet.txt
 
 # backup config and wallet to google bucket
-sudo gsutil cp -r /home/node_runner/.regen/config $1
-sudo gsutil cp /home/node_runner/wallet.txt $1
+sudo gsutil cp -r ~/.regen/config $1
+sudo gsutil cp ~/wallet.txt $1
 
 # delete file containing private key and keychain password
 rm wallet.txt
 
 # update config with peers for main chain
-sed -i '/persistent_peers =/c\persistent_peers = "69975e7afdf731a165e40449fcffc75167a084fc@104.131.169.70:26656,d35d652b6cb3bf7d6cb8d4bd7c036ea03e7be2ab@116.203.182.185:26656,ffacd3202ded6945fed12fa4fd715b1874985b8c@3.98.38.91:26656"' /home/node_runner/.regen/config/config.toml
+sed -i 's/persistent_peers = ""/persistent_peers = "69975e7afdf731a165e40449fcffc75167a084fc@104.131.169.70:26656,d35d652b6cb3bf7d6cb8d4bd7c036ea03e7be2ab@116.203.182.185:26656,ffacd3202ded6945fed12fa4fd715b1874985b8c@3.98.38.91:26656"/' ~/.regen/config/config.toml
 
 # the following line that changes localhost to 0.0.0.0 to expose the RPC port was written by regen
 # in their deployment guide. I have added it but left it commented out as this doesn't seem like something we want. 
-# sed -i 's#tcp://127.0.0.1:26657#tcp://0.0.0.0:26657#g' /home/node_runner/.regen/config/config.toml
+# sed -i 's#tcp://127.0.0.1:26657#tcp://0.0.0.0:26657#g' ~/.regen/config/config.toml
 
 # enable prometheus
-sed -i 's/prometheus = false/prometheus = true/' /home/node_runner/.regen/config/config.toml
+sed -i 's/prometheus = false/prometheus = true/' ~/.regen/config/config.toml
 
 # add regen to systemctl and start syncing
 echo "[Unit]
