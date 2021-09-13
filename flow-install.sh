d82a095efcb810513d804fec8b19ddfdf4274b67
SaltySousChef <hossein@funhaus.io>
2021-09-13 22:59:25 +0800

    Update flow-install.sh

--- flow-install.sh
+++ flow-install.sh
@@ -43,7 +43,14 @@ export DNS_ID="$(curl -X POST "https://api.cloudflare.com/client/v4/zones/$CLOUD
 
 # install dependencies
 sudo apt update
-sudo apt install build-essential libssl-dev -y
+sudo apt install build-essential libssl-dev apt-transport-https ca-certificates gnupg lsb-release -y
+
+# install docker
+sudo apt-get remove docker docker-engine docker.io containerd runc
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 
 # install go
 cd /tmp
